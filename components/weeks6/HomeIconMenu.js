3a293f8f4ea21d459340c7cb9434ebae3b8318af
chuthaphatketthed <150641578+chuthaphatketthed@users.noreply.github.com>
2024-09-05 16:25:50 +0700

    weeks9(2)

--- components/weeks6/HomeIconMenu.js
+++ components/weeks6/HomeIconMenu.js
@@ -23,7 +23,7 @@ export default function HomeIconMenu() {
       </View>
       <View style={{ flexDirection: "row", marginTop: 10 }}>
         <MyIcon title="Pokemon" name="bolt" size={30} color="orange" onPress={() => { navigation.navigate("PokemonTab"); }} />
-        <MyIcon title="xxx" name="question" size={30} color="orange" onPress={() => { navigation.navigate("xxx") }} />
+        <MyIcon title="Book Store" name="cubes" size={30} color="orange" onPress={ ()=>{ navigation.navigate("Book")}} />
         <MyIcon title="xxx" name="question" size={30} color="orange" onPress={() => { navigation.navigate("xxx") }} />
         <MyIcon title="xxx" name="question" size={30} color="orange" onPress={() => { navigation.navigate("xxx") }} />
       </View>
--- navigations/RootStack.js
+++ navigations/RootStack.js
@@ -4,6 +4,9 @@ import BottomTab from "./BottomTab";
 import PokemonTab from "./PokemonTab";
 import YourHealth from "../screens/week9/YourHealth";
 import TodoList from "../screens/week9/TodoList";
+import Book from "../screens/week9/Book";
+import BookDetail from "../screens/week9/BookDetail";
+import BookForm from "../screens/week9/BookForm";
 
 
 
@@ -14,9 +17,11 @@ export default function RootStack() {
     <Stack.Navigator initialRouteName="BottomTab">
       <Stack.Screen name="BottomTab" component={BottomTab} options={{ headerShown: false }} />
       <Stack.Screen name="PokemonTab" component={PokemonTab} options={{ title : "Pokemon World" }}  />
-      <Stack.Screen name="YourHealth" component={YourHealth} />
-      <Stack.Screen name="TodoList" component={TodoList} />
-
+      {/* <Stack.Screen name="YourHealth" component={YourHealth} />
+      <Stack.Screen name="TodoList" component={TodoList} /> */}
+      <Stack.Screen name="Book" component={Book} options={{ title: "Book" }} />
+      <Stack.Screen name="BookDetail" component={BookDetail} options={{ title: "Book Detail" }} />
+      <Stack.Screen name="BookForm" component={BookForm}   />
     </Stack.Navigator>
   );
 }
